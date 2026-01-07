667a91b3a3dd26222cebd656be3a3af216cc2ad1
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-07 11:52:58 +0530

    Update Navbar.js

--- main-code/components/Navbar.js
+++ main-code/components/Navbar.js
@@ -9,17 +9,17 @@ const Navbar = () => {
       </div>
       <div className='w-[49%] h-[99%] flex flex-row justify-center items-start gap-1'>
         <ul className='w-[60%] h-full flex flex-row justify-center items-center gap-2'>
-          <Link href="/"><li className='hover:text-lg text-[16px] font-sembold hover:text-[20px] hover:text-orange-900 hover:cursor-pointe w-[20%] h-[99%] flex justify-center items-center'>Home</li></Link>
-          <Link href="/about"><li className='hover:text-lg text-[16px] font-sembold hover:text-[20px] hover:text-orange-900 hover:cursor-pointer w-[20%] h-[99%] flex justify-center items-center'>About</li></Link>
-          <Link href="/generate"><li className='hover:text-lg text-[16px] font-sembold hover:text-[20px] hover:text-orange-900 hover:cursor-pointer w-[20%] h-[99%] flex justify-center items-center'>Shorten</li></Link>
-          <Link href="/contact"><li className='hover:text-lg text-[16px] font-sembold hover:text-[20px] hover:text-orange-900 hover:cursor-pointer w-[20%] h-[99%] flex justify-center items-center'>Contact US</li></Link>
+          <li className='hover:text-lg text-[16px] font-sembold hover:text-[20px] hover:text-orange-900 hover:cursor-pointe w-[99%] h-[99%] flex justify-center items-center'><Link href="/">Home</Link></li>
+          <li className='hover:text-lg text-[16px] font-sembold hover:text-[20px] hover:text-orange-900 hover:cursor-pointer w-[99%] h-[99%] flex justify-center items-center'><Link href="/about">About</Link></li>
+          <li className='hover:text-lg text-[16px] font-sembold hover:text-[20px] hover:text-orange-900 hover:cursor-pointer w-[99%] h-[99%] flex justify-center items-center'><Link href="/generate">Shorten</Link></li>
+          <li className='hover:text-lg text-[16px] font-sembold hover:text-[20px] hover:text-orange-900 hover:cursor-pointer w-[99%] h-[99%] flex justify-center items-center'><Link href="/contact">Contact US</Link></li>
         </ul>
         <div className='w-[40%] h-full flex flex-row justify-center items-center gap-2'>
           <div className='w-[49%] h-[99%] flex justify-center items-center'>
-            <Link href="/generate"><button className='w-[80%] h-[90%] rounded-[10px] bg-orange-500 hover:text-orange-900 hover:bg-orange-600'>Try now</button></Link>
+            <button className='w-[80%] h-[90%] rounded-[10px] bg-orange-500 hover:text-orange-900 hover:bg-orange-600'><Link href="/generate">Try now</Link></button>
           </div>
           <div className='w-[49%] h-[99%] flex justify-center items-center'>
-            <Link href="/gethub"><button className='w-[80%] h-[90%] rounded-[10px] bg-green-400 hover:bg-green-500 text-white'>Github</button></Link>
+            <button className='w-[80%] h-[90%] rounded-[10px] bg-green-400 hover:bg-green-500 text-white'><Link href="/github">Github</Link></button>
           </div>
         </div>
       </div>
