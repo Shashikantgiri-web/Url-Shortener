de74c0ab8217d4ff85808e00df21cee3a3817c31
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-07 19:47:06 +0530

    update

---
+++ main-code/app/generate/page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+
+const generate = () => {
+    const [url, setUrl] = useState('');
+    const [shortUrl, setShortUrl] = useState('');
+    const [generate, setGenerate] = useState(false)
+
+    const handleChange = (first) => {
+        
+    }
+
+  return (
+    <div className="w-[99%] h-auto flex flex-col justify-start items-center gap-4">
+        <h1>Generate Your Short URL</h1>
+        <input type="text" name="urlHere" id="a" placeholder='Enter your Url here' onClick={handleChange}/>
+        <input type="text" name="textHere" id="b" placeholder='Enter your perferred short Url text here' onClick={handleChange}/>
+        <button>Generate</button>
+    </div>
+  )
+}
+
+export default generate
--- main-code/app/page.js
+++ main-code/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Poppins } from "next/font/google";
+import Link from 'next/link'
 
 const poppins = Poppins({
   weight: ["400", "700"],
@@ -13,8 +14,12 @@ export default function Home() {
       <div className="w-[99%] h-auto flex flex-col justify-start items-center gap-4">
         <section className="w-full h-[50vh] grid grid-cols-2  bg-orange-200">
           <div className="w-[99%] h-full flex flex-col justify-center items-center">
-            <p className={`text-3xl font-bold ${poppins.className}`}>The best URL shortener in Market</p>
-            <p>We are the best straightforward URL shortener in the world</p>
+            <p className={`text-3xl font-bold w-[70%] ${poppins.className}`}>The best URL shortener in Market</p>
+            <p className="w-[70%]">We are the best straightforward URL shortener in the world</p>
+            <div className="w-[70%] flex flex-row justify-center items-center gap-4">
+               <button className='w-[40%] h-[90%] rounded-[10px] bg-orange-500 hover:text-white hover:bg-orange-600'><Link href="/generate">Try now</Link></button>
+               <button className='w-[40%] h-[90%] rounded-[10px] bg-green-400 hover:bg-green-500 text-white'><Link href="/github">Github</Link></button>
+            </div>
           </div>
           <div className="w-[99%] h-full flex justify-center items-center relative">
             <Image src="/vector.jpg" alt="URL Shortener" fill={true} className="mix-blend-darken" />
