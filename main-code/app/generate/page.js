35738b62e078f0e5afda2ed7a46dcf24fcec78dc
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-07 20:10:21 +0530

    update

--- main-code/app/generate/page.js
+++ main-code/app/generate/page.js
@@ -8,16 +8,12 @@ const generate = () => {
     const [shortUrl, setShortUrl] = useState('');
     const [generate, setGenerate] = useState(false)
 
-    const handleChange = (first) => {
-
-    }
-
   return (
-    <div className="w-[99%] h-auto flex flex-col justify-start items-center gap-4 mt-2">
-        <h1 className="text-2xl font-bold w-[40%]">Generate Your Short URL</h1>
-        <input type="text" name="urlHere" id="a" placeholder='Enter your Url here' onClick={handleChange} className='w-[40%] h-7.5 rounded-[10px] flex justify-start items-center pl-2 hover:bg-orange-200 text-white bg-transparent ring-2 ring-orange-700'/>
-        <input type="text" name="textHere" id="b" placeholder='Enter your perferred short Url text here' onClick={handleChange} className='w-[40%] h-7.5 rounded-[10px] flex justify-start items-center pl-2 hover:bg-orange-200 text-white bg-transparent ring-2 ring-orange-700'/>
-        <button className='w-[40%] h-7.5 rounded-[10px] bg-orange-700 text-white hover:bg-orange-600'>Generate</button>
+    <div className="w-[99%] h-auto flex flex-col justify-start items-center gap-4 mt-2 bg-orange-200">
+        <h1 className="text-2xl font-bold w-[40%] mb-1.5">Generate Your Short URL</h1>
+        <input type="text" name="urlHere" id="a" value={url} placeholder='Enter your Url here' onChange={(e) => setUrl(e.target.value)} className='w-[40%] h-7.5 rounded-[10px] flex justify-start items-center pl-2 hover:bg-orange-200 text-white bg-transparent ring-2 ring-orange-700'/>
+        <input type="text" name="textHere" id="b" value={shortUrl} placeholder='Enter your perferred short Url text here' onChange={(e) => setShortUrl(e.target.value)} className='w-[40%] h-7.5 rounded-[10px] flex justify-start items-center pl-2 hover:bg-orange-200 text-white bg-transparent ring-2 ring-orange-700'/>
+        <button className='w-[40%] h-10 rounded-[10px] bg-orange-500 hover:text-white hover:bg-orange-600 mt-1.5'>Generate</button>
     </div>
   )
 }
