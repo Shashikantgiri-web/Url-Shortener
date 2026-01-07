0779daddeb4133f59d49e846c5970b2ff32092c4
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-07 17:04:15 +0530

    Update page.js

--- main-code/app/page.js
+++ main-code/app/page.js
@@ -11,12 +11,14 @@ export default function Home() {
   return (
     <>
       <div className="main-page">
-        <section className="w-[99%] h-[50vh] grid grid-cols-2">
-          <div className="w-[50%] h-full flex flex-row justify-center items-center bg-orange-200">
+        <section className="w-[99%] h-[50vh] grid grid-cols-2  bg-orange-200">
+          <div className="w-[50%] h-full flex flex-row justify-center items-center">
             <p className="text-2xl font-bold">The best URL shortener in Market</p>
             <p>We are the best straightforward URL shortener in the world</p>
           </div>
-          <div className="w-[50%] h-full flex justify-center items-center bg-orange-200"></div>
+          <div className="w-[50%] h-full flex justify-center items-center relative">
+            <Image src="/vector.jpg" alt="URL Shortener" fill={true} className="mix-blend-darken" />
+          </div>
         </section>
       </div>
     </>
