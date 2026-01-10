dfd5b99c2ab2ec172d8075b8d5a1cda4b4aef7df
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-10 16:25:16 +0530

    Update route.js

--- main-code/app/api/generate/route.js
+++ main-code/app/api/generate/route.js
@@ -1,9 +1,15 @@
 import clientPromise from "@/lib/mongodb"
 
-export async function GET() {
+export async function POST(request) {
+  const body = await request.json()
   const client = await clientPromise
   const db = client.db("bitlines")
   const collection = db.collection("urls")
 
-  return Response.json({ message: 'Hello World' })
+  collection.insertOne({
+     url: body.url,
+     shortUrl: body.shortUrl,
+    })
+
+  return Response.json({ success: true, error:false, message: "URL generated successfully" })
 }
