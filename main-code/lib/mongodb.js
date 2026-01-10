505ed11607bf5e314de1a79ee357dc35351fc6e2
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-10 21:23:47 +0530

    Update mongodb.js

--- main-code/lib/mongodb.js
+++ main-code/lib/mongodb.js
@@ -3,26 +3,35 @@
 import { MongoClient } from 'mongodb'
 
 const uri = process.env.MONGODB_URI
-const options = { 
+const options = {
   useNewUrlParser: true,
 }
 
 let client
 let clientPromise
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Add Mongo URI to .env')
-}
-
-if (process.env.NODE_ENV === 'development') { 
+if (process.env.NODE_ENV === 'development') {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
+    if (!uri) {
+      console.warn('Warning: MONGODB_URI is not defined in developmental environment')
+      clientPromise = Promise.reject(new Error('Add Mongo URI to .env'))
+    } else {
+      client = new MongoClient(uri, options)
+      global._mongoClientPromise = client.connect()
+      clientPromise = global._mongoClientPromise
+    }
+  } else {
+    clientPromise = global._mongoClientPromise
   }
-  clientPromise = global._mongoClientPromise
 } else {
-  client = new MongoClient(uri, options)
-  clientPromise = client.connect()
+  if (!uri) {
+    // During build, MONGODB_URI might be missing. 
+    // We return a promise that rejects when called, but doesn't throw at module scope.
+    clientPromise = Promise.reject(new Error('Add Mongo URI to .env'))
+  } else {
+    client = new MongoClient(uri, options)
+    clientPromise = client.connect()
+  }
 }
 
 export default clientPromise
