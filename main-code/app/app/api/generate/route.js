1eb3accf1c574d4a287299d565f967f791494c25
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-11 12:17:27 +0530

    Update route.js

--- main-code/app/app/api/generate/route.js
+++ main-code/app/app/api/generate/route.js
@@ -11,20 +11,19 @@ export async function POST(request) {
     if (!client) {
       return Response.json({ success: false, error: "Database not connected" }, { status: 500 })
     }
-    const db = client.db("bitlines")
-    const collection = db.collection("urls")
+    const db = client.db("bitlinks")
+    const collection = db.collection("url")
+
+    // Check if the short url exists
     const doc = await collection.findOne({shorturl: body.shorturl})
     if(doc){
         return Response.json({success: false, error: true,  message: 'URL already exists!' })
     }
 
-    await collection.insertOne({
-      url: body.url,
-      shortUrl: body.shortUrl,
+    const result = await collection.insertOne({
+        url: body.url,
+        shorturl: body.shorturl
     })
-
-    // deploy again 
-    return Response.json({ success: true })
   } catch (error) {
     console.error(error)
     return Response.json({ success: false }, { status: 500 })
