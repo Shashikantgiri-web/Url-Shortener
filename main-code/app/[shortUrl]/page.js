d3bc00ebe04a683b15e9cc38aef123e931e5fdbf
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-12 20:41:24 +0530

    Update page.js

--- main-code/app/[shortUrl]/page.js
+++ main-code/app/[shortUrl]/page.js
@@ -19,10 +19,5 @@ export default async function Page({ params }) {
     //     redirect(`${process.env.NEXT_PUBLIC_BASE_URL}`)
     // }
 
-    return <div>
-            My Post: {shorturl}
-            <button onClick={()=>{console.log(`${shorturl}`)}} className="bg-blue-500 text-white px-4 py-2 rounded">
-                {shorturl}
-            </button>
-        </div>
+    return <div>My Post: {shorturl} </div>
 }
