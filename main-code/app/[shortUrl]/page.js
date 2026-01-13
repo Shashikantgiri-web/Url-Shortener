95f9cfa9d195e88f57ebf526f867dec30a015e04
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-13 11:41:32 +0530

    Update page.js

--- main-code/app/[shortUrl]/page.js
+++ main-code/app/[shortUrl]/page.js
@@ -3,7 +3,7 @@ import clientPromise from "@/lib/mongodb"
 
 
 export default async function Page({ params }) {
-    const shorturl = (await params)
+    const { shorturl } = await params
 
     // const client = await clientPromise;
     // const db = client.db("bitlinks")
