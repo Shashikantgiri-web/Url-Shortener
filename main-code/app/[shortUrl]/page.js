01631084c3ba169a76b7be22b54ccab9ecb33bd9
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-13 11:45:01 +0530

    Update page.js

--- main-code/app/[shortUrl]/page.js
+++ main-code/app/[shortUrl]/page.js
@@ -5,18 +5,18 @@ import clientPromise from "@/lib/mongodb"
 export default async function Page({ params }) {
     const { shorturl } = await params
 
-    // const client = await clientPromise;
-    // const db = client.db("bitlinks")
-    // const collection = db.collection("url")
+    const client = await clientPromise;
+    const db = client.db("bitlinks")
+    const collection = db.collection("url")
 
-    // const doc = await collection.findOne({shorturl: shorturl})
+    const doc = await collection.findOne({shorturl: shorturl})
     // console.log(doc)
-    // if(doc){
-    //      redirect(doc.url)
-    // }
-    // else{
-    //     redirect(`${process.env.NEXT_PUBLIC_BASE_URL}`)
-    // }
+    if(doc){
+         redirect(doc.url)
+    }
+    else{
+        redirect(`${process.env.NEXT_PUBLIC_BASE_URL}`)
+    }
 
     return <div>My Post: {shorturl} </div>
 }
