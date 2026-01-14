08a1049f2c7222dbe39f20da814f17849d7f6979
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-14 08:18:13 +0530

    Update page.js

--- main-code/app/[shortUrl]/page.js
+++ main-code/app/[shortUrl]/page.js
@@ -3,23 +3,24 @@ import clientPromise from "@/lib/mongodb"
 
 
 export default async function Page({ params }) {
-    const shorturl = params.shorturl
-    const client = await clientPromise
-    if (!client) {
-      return Response.json({ success: false, error: "Database not connected" }, { status: 500 })
-    }
-    const db = client.db("bitlinks")
-    const collection = db.collection("url")
+  const shortUrl = params.shortUrl
+  const client = await clientPromise
+  if (!client) {
+    return Response.json({ success: false, error: "Database not connected" }, { status: 500 })
+  }
+  const db = client.db("bitlinks")
+  const collection = db.collection("url")
 
-    // Check if the short url exists
-    const doc = await collection.findOne({ shortUrl: shorturl })
-    if (doc) {
-      redirect(doc.url)
-    }
-    else {
-        redirect(`${process.env.NEXT_PUBLIC_BASE_URL}`)
-    }
+  // Check if the short url exists
+  const doc = await collection.findOne({ shortUrl: shortUrl })
+  if (doc) {
+    redirect(doc.url)
+  }
+  else {
+    // redirect(`${process.env.NEXT_PUBLIC_BASE_URL}`)
+    return Response.json({ success: false, error: "Url Not Found" })
+  }
 
-    
-    return <div>my post : {shorturl}</div>
+
+  return <div>my post : {shortUrl}</div>
 }
