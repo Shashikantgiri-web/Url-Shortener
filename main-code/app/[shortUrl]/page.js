976e23a86c5711f97cee8c56a7052da4fb48e21a
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-14 08:05:26 +0530

    Update page.js

--- main-code/app/[shortUrl]/page.js
+++ main-code/app/[shortUrl]/page.js
@@ -3,19 +3,23 @@ import clientPromise from "@/lib/mongodb"
 
 
 export default async function Page({ params }) {
-    const { shorturl } = await params
-
-    const client = await clientPromise;
+    const shorturl = params.shorturl
+    const client = await clientPromise
+    if (!client) {
+      return Response.json({ success: false, error: "Database not connected" }, { status: 500 })
+    }
     const db = client.db("bitlinks")
     const collection = db.collection("url")
 
+    // Check if the short url exists
     const doc = await collection.findOne({ shortUrl: shorturl })
     if (doc) {
-        redirect(doc.url)
-        // return <iframe src={doc.url} frameborder="0" allowfullscreen></iframe>
+      redirect(doc.url)
     }
     else {
         redirect(`${process.env.NEXT_PUBLIC_BASE_URL}`)
     }
+
+    
     return <div>my post : {shorturl}</div>
 }
